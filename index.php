66b18b178247dd2080afe84117d120f7904c3c7c
Vedant <vedant_raj@yahoo.co.in>
2023-10-31 16:12:09 +0530

    Web page

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<?php
+$articalName = "AYURVEDIC MANAGEMENT OF RHEUMATOID ARTHRITIS: A CASE STUDY";
+$manuscriptNo = "WJPMR/6656/9/2023";
+$authors = "Dr. Shraddha Saraf, Dr. Swati Nagpal, Dr. Rajesh Meshram and Dr. Shwetal Shivhare";
+$reviewerCount = "three";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.5">
+    <title>WJPMR Certificate</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="p-3">
+    
+    <main class="d-flex justify-center border border-primary p-4 bg-secondary-subtle">
+
+        <div class="mx-auto">
+            <img src="bgimg.png" width="1029" height="726.6">
+
+            <div class="contentWdth contentPosition mx-auto font_style content_style lh-lg">
+                <p>This is to certify that Article entitled <span style="font-weight: bold"> "<?php echo $articalName ?>"</span>, Manuscript no.  <?php echo $manuscriptNo ?>, <span style="font-weight: bold"> <?php echo $authors ?></span>, has been published in World Journal of Pharmaceutical and Medical Research, (Volume 9, Issue 10.) after getting reviewed by  <?php echo $reviewerCount ?> reviewers.
+                </p>
+            </div>
+        </div>
+
+    </main>
+
+</body>
+</html>
